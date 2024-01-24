fb65d063180d62568c5a3feb2144262134de357c
saisashank <saisai66242@gmail.com>
2024-01-24 09:54:15 +0530

    my seond commit

---
+++ insurance.java
@@ -0,0 +1,22 @@
+class policy
+{
+ public static void main (String args[])
+ {
+   String health="excellent";
+   String gender="male";
+   String location="city";
+   int policy=200000,maxvalue=0,age=30;
+   int premium=0;
+  if((health=="excellent")&&(gender=="male")&&(location=="city")&&(age>=25&&age<=35)&&(policy<=200000))
+   premium=4*policy/1000;
+  else if((health=="excellent")&&(gender=="female")&&(location=="city")&&(age>=25&&age<=35)&&(policy<=100000))
+    premium=3*policy/1000;
+  else if((health=="poor")&&(gender=="male")&&(location=="village")&&(age>=25&&age<=35)&&(policy<=10000))
+     premium=6*policy/1000;
+  else
+   System.out.println("not secured");
+ System.out.println(premium);
+ maxvalue=premium+policy;
+ System.out.println(maxvalue);
+ }
+}
